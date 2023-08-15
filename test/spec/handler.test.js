5d3d07a7521eedcc96b83aa24c4850ab3e8b0596
Huillca Reynaldo <lymreynaldo@icloud.com>
2023-08-14 23:27:12 -0300

    test: add password field valid

--- test/spec/handler.test.js
+++ test/spec/handler.test.js
@@ -33,13 +33,50 @@ const getFirstCallArg = ({ mock }) => mock.calls[0].arguments[0];
 
 describe("api unit test suite", () => {
   describe("/login", () => {
-    it("should receive not authorized when user or password is invalid", async (context) => {
+    it("should receive not authorized when user is invalid", async (context) => {
       const inputRequest = mockRequest({
         url: "/login",
         method: "POST",
         body: {
           user: "invalid",
           password: "",
+        },
+      });
+      const outputReponse = mockResponse({
+        mockContext: context.mock,
+      });
+      await handler(inputRequest, outputReponse);
+      const expected = 401;
+      assert.strictEqual(
+        getFirstCallArg(outputReponse.writeHead),
+        expected,
+        `should receive 401 status code, received ${getFirstCallArg(
+          outputReponse.writeHead
+        )}`
+      );
+
+      const expectedResponse = JSON.stringify({ error: "user invalid!" });
+      assert.strictEqual(
+        outputReponse.end.mock.callCount(),
+        1,
+        "should call response.end once"
+      );
+      assert.strictEqual(
+        getFirstCallArg(outputReponse.end),
+        expectedResponse,
+        `should receive ${expectedResponse}, received ${getFirstCallArg(
+          outputReponse.end
+        )}`
+      );
+    });
+
+    it("should receive not authorized when password is invalid", async (context) => {
+      const inputRequest = mockRequest({
+        url: "/login",
+        method: "POST",
+        body: {
+          user: "devolda",
+          password: "1234",
         },
       });
       const outputReponse = mockResponse({
